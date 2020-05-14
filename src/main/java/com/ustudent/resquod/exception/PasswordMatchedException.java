0ebf01b0c046c67d6c30ab3a47dd29ad2d3de2de
patryk.zaczkiewicz@gmail.com <6hh7dfqxX>
2020-05-14 23:34:14 +0200

    added method for changing password and validation of user data update

---
+++ src/main/java/com/ustudent/resquod/exception/PasswordMatchedException.java
@@ -0,0 +1,6 @@
+package com.ustudent.resquod.exception;
+
+public class PasswordMatchedException extends RuntimeException {
+    public PasswordMatchedException() {
+    }
+}
---
+++ src/main/java/com/ustudent/resquod/model/dao/UserPassword.java
@@ -0,0 +1,29 @@
+package com.ustudent.resquod.model.dao;
+
+public class UserPassword {
+
+    private String oldPassword;
+    private String newPassword;
+
+    public UserPassword(String oldPassword, String newPassword){
+        this.newPassword=newPassword;
+        this.oldPassword=oldPassword;
+    };
+
+    public String getOldPassword() {
+        return oldPassword;
+    }
+
+    public void setOldPassword(String oldPassword) {
+        this.oldPassword = oldPassword;
+    }
+
+    public String getNewPassword() {
+        return newPassword;
+    }
+
+    public void setNewPassword(String newPassword) {
+        this.newPassword = newPassword;
+    }
+
+}
