caaa7dd801e827217f95a0a8451546330f55d901
patryk.zaczkiewicz@gmail.com <6hh7dfqxX>
2020-06-15 00:48:01 +0200

    Added import to repair

--- src/main/java/com/ustudent/resquod/controller/PositionController.java
+++ src/main/java/com/ustudent/resquod/controller/PositionController.java
@@ -14,6 +14,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
+import java.util.List;
+
 @RestController
 @Api(value = "Position Management")
 public class PositionController {
